0eb944ccdf4d80d8a05156d8df30380315f49cd9
Nikita <perminovni@gmail.com>
2025-03-14 10:11:20 +0300

    default crypto table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import {useState} from 'react'
 import './App.css'
 import CryptoList from "./Components/CryptoList";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
--- src/Components/CryptoList/index.tsx
+++ src/Components/CryptoList/index.tsx
@@ -2,8 +2,30 @@ import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
 import styles from './CryptoList.module.css'
 
+
+
 export default function CryptoList() {
     const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&page=1`
+
+
+    function translateMarketCap(marketCap: number): string {
+        if (marketCap >= 1e12) {
+            // Триллионы
+            return `$${(marketCap / 1e12).toFixed(2)}T`;
+        } else if (marketCap >= 1e9) {
+            // Миллиарды
+            return `$${(marketCap / 1e9).toFixed(2)}B`;
+        } else if (marketCap >= 1e6) {
+            // Миллионы
+            return `$${(marketCap / 1e6).toFixed(2)}M`;
+        } else if (marketCap >= 1e3) {
+            // Тысячи
+            return `$${(marketCap / 1e3).toFixed(2)}K`;
+        } else {
+            // Меньше тысячи
+            return `$${marketCap.toLocaleString()}`;
+        }
+    }
 
     async function fetchData() {
         const response = await axios.get(url)
@@ -21,19 +43,45 @@ export default function CryptoList() {
     if (isLoading) return <p>Загрузка...</p>;
     if (isError) return <p>Ошибка: {error.message}</p>;
     return (
-        <>
-            <h3>Курс криптовалют</h3>
-            <div className={styles.cryptoContainer}>
-                {data?.map((item) => (
-                    <div key={item.id} className={styles.cryptoItem}>
-                        <img className={styles.cryptoImg} src={item.image} alt=""/>
-                        <h5>{item.name}</h5>
-                        <h6>{item.symbol}</h6>
-                        <h5>{item.current_price}</h5>
-                    </div>
+        <div className={styles.container}>
+            <table className={styles.table}>
+                <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Market Cap</th>
+                    <th>Market change</th>
+                    <th>Price change</th>
+                    <th>Low</th>
+                    <th>High</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.market_cap_rank} </td>
+                        <td className={styles.cryptoNameBar}>
+                            <img className={styles.cryptoImage} src={item.image} alt=""/>
+                            <div className={styles.smallCryptoNameBar}>
+                                <p className={styles.cryptoName}>{item.name}</p>
+                                <p className={styles.cryptoSymbol}>{item.symbol}</p>
+                            </div>
+                        </td>
+                        <td className={styles.defaultText}>${item.current_price}</td>
+                        <td>{translateMarketCap(item.market_cap)}</td>
+                        <td className={item.market_cap_change_percentage_24h < 0 ? styles.negative : styles.positive}>
+                            {item.market_cap_change_percentage_24h}%
+                        </td>
+                        <td className={item.price_change_percentage_24h < 0 ? styles.negative : styles.positive}>
+                            {item.price_change_percentage_24h}%
+                        </td>
+                        <td>${item.low_24h}</td>
+                        <td>${item.high_24h}</td>
+                    </tr>
                 ))}
-            </div>
-        </>
-
-    )
+                </tbody>
+            </table>
+        </div>
+    );
 }
