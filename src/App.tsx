5dc4d940274be692729322dd75890a70a3426679
Nikita <perminovni@gmail.com>
2025-03-22 21:01:02 +0300

    Added router, testing working with branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import CryptoList from "./Components/CryptoList";
+import {Routes, Route} from "react-router";
 
 function App() {
 
     return (
         <>
-            <CryptoList />
+            <Routes>
+                <Route path="/" element={<CryptoList />} />
+                <Route path="chat" element={"chat"} />
+            </Routes>
         </>
     )
 }
